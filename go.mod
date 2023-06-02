785c13d0c6ba80cd598a0a708b30ed7dd0603e41
yuanchenhao <yxb0926@163.com>
2023-06-02 13:33:48 +0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module submodule-test
+
+go 1.20
+
+require gitlab.bj.sensetime.com/cicd-helloworld/test-module v0.0.0-20230602041444-e5debb82c697
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "gitlab.bj.sensetime.com/cicd-helloworld/test-module"
+    "fmt"
+)
+
+func main() {
+    fmt.Println("Hello", testgo.MyName())
+}
